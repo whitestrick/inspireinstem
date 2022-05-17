99be2ab4646a584c29fef40a446a2912eb85fd76
whitestrick <mwangiaugustine08@gmail.com>
2022-05-17 12:04:24 +0300

    lesson2 about integers

---
+++ lesson2.py
@@ -0,0 +1,15 @@
+#python basics
+
+#this is a comment
+
+#adding numbers a and b
+#this is for a
+#multi/manylines
+#comments
+print ("my name is kaguma")
+print ("i am from nairobi")
+print ("i like cycling")
+print ("i'm going swimming on saturday ")
+print ("i play water polo")
+
+
