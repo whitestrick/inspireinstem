6d39cac3d3a1a33576ed57c604b8218583b7baba
whitestrick <mwangiaugustine08@gmail.com>
2022-05-18 11:14:49 +0300

    lesson3

---
+++ asignment1.2.py
@@ -0,0 +1,4 @@
+#calculate the volume of a cube
+side = input("enter the side of the cube")
+prod =int(side)*int(side)*int(side)
+print("the volume of the cube is " + str(prod))
---
+++ slice.py
@@ -0,0 +1,52 @@
+#multiple lines
+msg = "QRWE23341 Confirm you have recieved 99999 from Ada Lover on 8th may 2022"
+print(msg)
+
+txt = """Hello i am ghost,
+i come from coast
+i love swimming"""
+print(txt)
+
+
+city = "Nairobi"
+print(city[:5])
+print(city[2:])
+
+
+
+f_name ="kaguma paul" # small letters
+# .upper()convert to upper class method
+print(f_name.upper())
+
+s_name = "KAGUMA PAUL" 
+# .lower() convert to lower case
+print(s_name.lower())
+
+
+#concatinating "converting one data type to another"
+#int ->float float(x)
+#float ->int int(x)
+#int->string str(x)
+number=6
+print(str(number))
+
+x=4
+print(float(x))
+
+y=3.24
+print(int(y))
+
+f_name = "kaguma"
+
+s_name =" ghost"
+full_name =f_name + s_name
+print(full_name)
+
+#the replace() method replaces a string with another
+
+name = "Harley Queen"
+print(name.replace('l','g'))
+msg = "hello from sherlock holmes how are you"
+print(msg.split())
+
+print(len(msg))
