6ffbb442b1a40ff3ea69c48bb668629ff3b8725c
whitestrick <mwangiaugustine08@gmail.com>
2022-05-18 11:34:38 +0300

    lesson 5

---
+++ lesson5(1).py
@@ -0,0 +1,13 @@
+#methods
+name = "kaguma ghost"
+name = "Ada lovelace"
+user_name =" lovelace"
+age = 19
+age = 55
+person ="i am"+str(name)"add i am"+str(age)+"years old."
+#print(person)
+#the fomart methods
+#print("my name is{} and i am{} years old".fomart(name,age))
+#newline\n andtab \t
+#print(f"my name(name)\n and i am(age) years old")
+print(name.strip(),age)
