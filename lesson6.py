d1c05954477b8a837eb5be702c06e3b5378b5460
whitestrick <mwangiaugustine08@gmail.com>
2022-05-19 11:49:04 +0300

    lesson6

---
+++ lesson6.py
@@ -0,0 +1,36 @@
+#learning about list
+#motorcycle_owner = input("enter motor cycle owner")
+plate_number = ['H1234','D1234','K1234']
+motorcycle = ['honda','densei','kawasaki']
+dash = " is "
+#acessing list items using index
+#print(motorcycle)
+#print(motorcycle[0])
+#motorcycle [2] = "bugati"#changing element in a list
+#adding element in a list --.append
+#motorcycle.append('bugati')#adding items to a list
+#print(motorcycle[0] + dash + plate_number[0])
+#print(motorcycle[1] + dash + plate_number[1])
+#print(motorcycle[2] + dash + plate_number[2])
+#deleting items from a list --del
+# del motorcycle[0]
+# print(motorcycle)
+#popped_motorcycle = motorcycle.pop
+#print(motorcycle.pop)
+#print(popped_motorcycle)
+#task print statement
+#my name is mojo jojo and i own a motorcycle plate plate_number
+#print("my name is {} \nI own a {} plates {}".format(motorcycle_owner,motorcycle[2],plate_number[2]))
+#removing items from a list
+#motorcycle.remove("honda")
+#print(motorcycle)
+
+
+#loops.py
+school=['Joy','Hope','Mercy','Happy']
+pupil=['Peace','Ptience','Amani','Character']
+print(pupil[0] + " goes to " +school[0])
+for pupil in pupil:
+    
+ print(f'Hello i am {pupil}')
+ print(f'hello i am {pupil} ')
