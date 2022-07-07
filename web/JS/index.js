4439a030162931f2763475fe69f5af67aa507d63
whites_strick <105627668+whitestrick@users.noreply.github.com>
2022-07-07 09:24:00 +0300

    Add files via upload

---
+++ web/JS/index.js
@@ -0,0 +1,13 @@
+let message="Hello World"
+console.log(message)
+//single line coment
+/*
+multiline comment in javascript
+*/
+let num1=134
+let num2=134
+console.log(num1+num2)
+function SubmitButton(){
+alert("Button has been clicked")
+
+}
